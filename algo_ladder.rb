65491d7866b1381f3d49493c4f670447b90a2100
Mark Francisco <mfrancisco581@gmail.com>
2021-06-03 20:16:11 -0500

    add leap year algo problem

--- algo_ladder.rb
+++ algo_ladder.rb
@@ -151,3 +151,38 @@
 
 # pp count_hash_items(["Dewey", "Truman", "Dewey", "Dewey", "Truman", "Truman", "Dewey", "Truman", "Truman", "Dewey", "Dewey"])
 # pp count_hash_items(["barry", "joe", "joe", "barry", "barry", "joe", "jello"])
+
+##################################################################
+##################################################################
+
+# # Given a year, report if it is a leap year.
+# # The tricky thing here is that a leap year in the Gregorian calendar occurs:
+# # on every year that is evenly divisible by 4
+# #   except every year that is evenly divisible by 100
+# #     unless the year is also evenly divisible by 400
+
+# # For example, 1997 is not a leap year, but 1996 is. 1900 is not a leap year, but 2000 is.
+# # If your language provides a method in the standard library that does this look-up, pretend it doesn't exist and implement it yourself.
+
+# # 1997 => false
+# # 1996 => true
+# # 1992 => true
+# # 2000 => true
+# # 2100 => false
+# # 400 BC => true
+
+# # leap year if: divisible by 4 && (divisible by 400 && divisible by 100)
+
+# def leap_year?(year)
+#   if (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)
+#     return true
+#   else
+#     return false
+#   end
+# end
+
+# pp leap_year?(1997) #=> false
+# pp leap_year?(1996) #=> true
+# pp leap_year?(2000) #=> true
+# pp leap_year?(2100) #=> false
+# pp leap_year?(-400) #=> true
